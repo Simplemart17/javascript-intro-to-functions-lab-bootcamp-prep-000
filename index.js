01e26d12e00d5a41c443122a43d010c76cb6843c
Martins Aloba <aloba.martins@gmail.com>
2018-09-03 12:46:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,12 +30,7 @@ function sayHiToGrandma(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()) {
-    return "I can't hear you!"
-  }
-  if (string === string.toUpperCase()) {
-    return "YES INDEED!"
-  }
   if (string === "I love you, Grandma." {
     return "I love you, too."
+  
 }
